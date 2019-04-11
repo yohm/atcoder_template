0530fab4477dc8b35c086ad3c10098e5894d7ad6
Yohsuke Murase <yohsuke.murase@gmail.com>
2019-04-11 22:50:25 +0900

    updated template

--- b.cpp
+++ b.cpp
@@ -1,42 +1,19 @@
 #include <iostream>
 #include <cstdint>
 #include <vector>
+#include <map>
+#include <queue>
+#include <set>
+#include <algorithm>
 #include <cassert>
 
 using namespace std;
-typedef int64_t i_t;
+typedef int64_t SI;
+typedef uint64_t UI;
 
-void read(i_t& N, vector<i_t>& V) {
-  cin >> N;
-  V.resize(N);
-  for(size_t i=0; i<N; i++) {
-    i_t x;
-    cin >> x;
-    V[i] = x;
-  }
-}
-
-int solve(const vector<i_t>& V) {
-  return 0;
-}
-
-void test() {
-  {
-    vector<i_t> V = {0,1,2};
-    assert(solve(V)==0);
-  }
-  cerr << "OK" << endl;
-}
+using namespace std;
 
 int main() {
-  /*
-  vector<i_t> V;
-  i_t N;
-  read(N, V);
-  int ans = solve(V);
-  cout << ans << endl;
-  */
-  test();
 
   return 0;
 }
